62d7413f5f9ac4bed72fc39612b305253b0ef378
AlexRos101 <ros1996101@gmail.com>
2021-11-13 16:04:43 +0800

    feature: Add event synchronization feature

--- common/config.js
+++ common/config.js
@@ -10,19 +10,19 @@ const config = {
     serviceDelay: 5000,
 
     historyURL:
-        'https://api-testnet.bscscan.com/api?module=account&action=txlist&address=CONTRACT_ADDRESS&' + 
+        'https://api-testnet.bscscan.com/api?module=account&action=txlist&address=CONTRACT_ADDRESS&' +
         'startblock=START_BLOCK&endblock=99999999&sort=asc&apikey=36XTKGWV92Q7B9FEKMRJRXQH5UBG3C2ANE',
 
     contractArcadeDoge: '0xEA071968Faf66BE3cc424102fE9DE2F63BBCD12D',
     contractBUSD: '0x8301f2213c0eed49a7e28ae4c3e91722919b8b47',
-    contractSwap: '0x8765194B78de8fEf50f862dAe23814352043b13b',
+    contractSwap: '0xe4713448b77447B3f1C5BC7CC3eB1264A14Caec6',
 
     backendKey: 'ArcadeDogeBackend',
     gameBackendUrl: 'http://localhost:4001',
 
-    eventURL: 
-        'https://api-testnet.bscscan.com/api?module=logs&action=getLogs&fromBlock=START_BLOCK&toBlock=latest&' + 
-        'address=CONTRACT_ADDRESS'
+    eventURL:
+        'https://api-testnet.bscscan.com/api?module=logs&action=getLogs&fromBlock=START_BLOCK&toBlock=latest&' +
+        'address=CONTRACT_ADDRESS',
 };
 
 module.exports = config;
