2f691f2183e8eb6ffc6aad762380026cf48217a7
AlexRos101 <ros1996101@gmail.com>
2021-11-22 04:11:05 +0800

    config: Update swap contract address

--- common/config.js
+++ common/config.js
@@ -15,7 +15,7 @@ const config = {
 
     contractArcadeDoge: '0xEA071968Faf66BE3cc424102fE9DE2F63BBCD12D',
     contractBUSD: '0x8301f2213c0eed49a7e28ae4c3e91722919b8b47',
-    contractSwap: '0x608C9c5de562960744484B2073BF7e0d3645AeAC',
+    contractSwap: '0xbB85c903178FF4444FED1C61b105BeDaba7E9027',
 
     backendKey: 'ArcadeDogeBackend',
     gameBackendUrl: 'http://localhost:4001',
